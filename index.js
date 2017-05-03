2ee0e776439d2e819c95b96bd4873ea7113ad9ba
Tyler Long <tyler4long@gmail.com>
2017-05-03 11:20:08 +0800

    First try of glip socket.io

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 require('dotenv').config()
+const GlipSocket = require('glip.socket.io')
 
-console.log(process.env.GLIP_HOST)
-console.log(process.env.GLIP_EMAIL)
-console.log(process.env.GLIP_PASSWORD)
+const client = new GlipSocket({
+  host: process.env.GLIP_HOST,
+  port: process.env.GLIP_PORT,
+  user: process.env.GLIP_EMAIL,
+  password: process.env.GLIP_PASSWORD
+})
+
+client.on('message', (type, data) => {
+  if (type === this.client.type_ids.TYPE_ID_POST && data.text === 'ping') {
+    client.post(data.group_id, 'pong')
+  }
+})
+
+client.start()
