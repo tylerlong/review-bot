605446377515558344651a8c1189fa550e9e90af
Tyler Long <tyler4long@gmail.com>
2017-05-18 15:24:44 +0800

    Fix a typo

--- src/common/db.js
+++ src/common/db.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const loadDb = (dbName = 'ios') => {
   const dbPath = path.join(__dirname, `../../db/${dbName}.json`)
-  if (!fs.existsSyncdbPath) {
+  if (!fs.existsSync(dbPath)) {
     fs.writeFileSync(dbPath, '{}')
   }
   return JSON.parse(fs.readFileSync(dbPath))
