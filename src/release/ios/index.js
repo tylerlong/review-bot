7f1ce311c811cefa1c0f5692ae49761e26fea082
Tyler Long <tyler4long@gmail.com>
2017-09-11 18:42:50 +0800

    Skeleton for iOS release

--- src/release/ios/index.js
+++ src/release/ios/index.js
@@ -1,14 +1,51 @@
+const { CronJob } = require('cron')
+
 const client = require('../../common/glip')
 const engine = require('../common/nunjucks')
 const { loadDb, saveDb } = require('./db')
+const { lookup } = require('./spider')
 
-const RINGCENTRAL_APPS = {
-  glip: 715886894,
-  ringcentral: 293305984,
-  meetings: 688920955
-}
+/*
+
+ATT app:
+- https://itunes.apple.com/us/app/ringcentral-office-hand-from-at-t/id398089358?mt=8
+- ID = 398089358
+http://itunes.apple.com/lookup?id=398089358
+
+BT Cloud Phone
+- https://itunes.apple.com/gb/app/bt-cloud-phone/id948639434?mt=8
+- ID = 948639434
+http://itunes.apple.com/gb/lookup?id=948639434
+
+TELUS Business Connect
+- https://itunes.apple.com/ca/app/telus-business-connect/id925058301?mt=8
+- ID = 925058301
+http://itunes.apple.com/lookup?id=925058301
+
+*/
 
 const db = loadDb()
+
+const monitors = {}
+
+const cronJob = async (group, app) => {
+  let appInfo = await lookup(app)
+  // todo:
+  // compare release number
+  // send notification to Glip
+  // save latest release number to db
+  saveDb(db)
+}
+
+Object.keys(db).forEach((group) => {
+  monitors[group] = {}
+  Object.keys(db[group]).forEach((app) => {
+    monitors[group][app] = new CronJob('0 */30 * * * *', () => {
+      cronJob(group, app)
+    })
+    monitors[group][app].start()
+  })
+})
 
 client.on('message', async (type, data) => {
   if (type !== client.type_ids.TYPE_ID_POST) {
@@ -28,5 +65,49 @@ client.on('message', async (type, data) => {
     })
     const message = engine.render('ios/apps.njk', { apps })
     client.post(group, message)
+    return
+  }
+
+  // ios add
+  let match = data.text.match(/^release ios add (.+?)$/)
+  if (match !== null) {
+    const app = match[1].trim()
+    const appInfo = await lookup(app)
+    const name = appInfo[0]['im:name'].label
+    const latestRelease = appInfo['latestRelease']
+    db[group][app] = { name, latestRelease }
+
+    monitors[group] = monitors[group] || {}
+    if (monitors[group][app]) {
+      monitors[group][app].stop()
+      delete monitors[group][app]
+    }
+    monitors[group][app] = new CronJob('0 */10 * * * *', () => {
+      cronJob(group, app)
+    })
+    monitors[group][app].start()
+
+    client.post(group, 'done')
+    saveDb(db)
+    return
+  }
+
+  // ios remove
+  match = data.text.match(/^release ios remove (.+?)$/)
+  if (match !== null) {
+    const app = match[1].trim()
+    delete db[group][app]
+    monitors[group][app].stop()
+    delete monitors[group][app]
+    client.post(group, 'done')
+    saveDb(db)
+    return
+  }
+
+  // ios release
+  match = data.text.match(/^release ios ([a-z0-9]+)$/)
+  if (match !== null) {
+    const app = match[1].trim()
+    // todo: send release number to Glip
   }
 })
